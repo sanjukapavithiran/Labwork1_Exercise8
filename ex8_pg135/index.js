92c47cdadc3af56c736cc8b9d19a98196417c2d2
sanjukapavithiran <sanjukapavithiran@gmail.com>
2024-02-15 14:57:47 +1300

    Commit updated index.js

--- ex8_pg135/index.js
+++ ex8_pg135/index.js
@@ -5,29 +5,35 @@ var elComeOut = document.getElementById('roll');
 elComeOut.onclick = function () { rollDice(); };
 
 function rollDice() {
+    var numSidesInput = prompt("Enter the number of sides for the dice:");
+    var numSides = parseInt(numSidesInput);
 
-    var diceOne = dicegenerator(6)
-    var diceTwo = dicegenerator(6)
+    if (isNaN(numSides) || numSides <= 0) {
+        console.log("Invalid input. Please enter a positive number for the number of sides.");
+        return; // Stop execution if input is invalid
+    }
+
+    var diceOne = dicegenerator(numSides);
+    var diceTwo = dicegenerator(numSides);
 
     console.log(diceOne + ' ' + diceTwo);
 
-    for (var i = 1; i <= 6; i++) {
+    for (var i = 1; i <= numSides; i++) {
         elDiceOne.classList.remove('show-' + i);
         if (diceOne === i) {
             elDiceOne.classList.add('show-' + i);
         }
     }
 
-    for (var k = 1; k <= 6; k++) {
+    for (var k = 1; k <= numSides; k++) {
         elDiceTwo.classList.remove('show-' + k);
         if (diceTwo === k) {
             elDiceTwo.classList.add('show-' + k);
         }
     }
-    setTimeout(rollDice(), 1000);
 }
 
 function dicegenerator(numSides){
     let result = Math.floor((Math.random() * numSides) + 1);
-    return result
+    return result;
 }
